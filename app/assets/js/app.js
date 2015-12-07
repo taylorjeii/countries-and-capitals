453f05156c5a9fb86dfb047acb2aa9f269901159
James Taylor <jtaylor@mcdpartners.com>
2015-12-07 16:52:02 -0500

    trying to get app working

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -9,7 +9,7 @@ var app =  angular.module('app', [
 app.config(['$routeProvider', function ($routeProvider){
   $routeProvider.when('/', {
     templateUrl: 'partials/home.html',
-    controller: 'MainController'
+    controller: 'HomeController'
   })
   .when('/countries', {
     templateUrl: 'partials/countriesList.html',
@@ -22,24 +22,49 @@ app.config(['$routeProvider', function ($routeProvider){
   })
   .when('/details/:country', {
     templateUrl: 'partials/country.html',
-    controller: 'MainController'
+    controller: 'CountryListController'
   })
   .otherwise({
     redirectTo: '/'
   });
 }]);
 
+// Main Controller Setup
+app.controller('HomeController', ['$scope', HomeController]);
+
+function HomeController ($scope){
+
+ }
 
  // Main Controller Setup
 app.controller('MainController', ['$scope', '$routeParams' , 'countriesData', MainController]);
 
 function MainController ($scope, $routeParams, countriesData){
+    console.log(countriesdata);
     $scope.countries = countriesData;
     $scope.country = $routeParams.country;
     $scope.area = '';
     $scope.capital = '';
     $scope.capitalPop = '';
     $scope.neighbors = '';
+
+    // dataService.getAllCountries()
+    //   .then(function(result){
+    //     $scope.countries = result.geonames;
+    //     console.log($scope.countries);
+    //   });
+ }
+
+ // Main Controller Setup
+app.controller('CountryListController', ['$scope', '$routeParams' , CountryListController]);
+
+function CountryListController ($scope, $routeParams){
+    console.log(countriesdata);
+    $scope.country = $routeParams.country;
+    // $scope.area = '';
+    // $scope.capital = '';
+    // $scope.capitalPop = '';
+    // $scope.neighbors = '';
 
     // dataService.getAllCountries()
     //   .then(function(result){
